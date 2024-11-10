67eafb696da033b4849e7b029f0445c9d6e09307
hakushun <hakushun.pianist@gmail.com>
2024-11-10 11:03:04 +0900

    refactor: 無駄なstateを削除

--- app/components/QuestionList/index.tsx
+++ app/components/QuestionList/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import styles from './index.module.css';
 import { QuestionItem } from '../QuestionItem';
 import { Question } from '../../lib/mock/questions';
@@ -11,14 +11,22 @@ type Props = {
 
 export function QuestionList({ questions }: Props) {
   const questionRefs = useRef<HTMLElement[]>([]);
-  const [targetIndex, setTargetIndex] = useState(0);
+
+  const focusQuestion = (index: number) => {
+    const nextQuestionRef = questionRefs.current[index];
+
+    if (!nextQuestionRef) return;
+
+    nextQuestionRef.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    nextQuestionRef.focus();
+  };
 
   const handleFocus = (id: number) => {
     const index = questions.findIndex((question) => question.id === id);
 
     if (index === -1) return;
 
-    setTargetIndex(index);
+    focusQuestion(index);
   };
 
   const handleFocusNext = (id: number) => {
@@ -26,17 +34,8 @@ export function QuestionList({ questions }: Props) {
 
     if (index === -1) return;
 
-    setTargetIndex(index + 1);
+    focusQuestion(index + 1);
   };
-
-  useEffect(() => {
-    const nextQuestionRef = questionRefs.current[targetIndex];
-
-    if (!nextQuestionRef) return;
-
-    nextQuestionRef.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    nextQuestionRef.focus();
-  }, [targetIndex]);
 
   return (
     <ul className={styles.list}>
