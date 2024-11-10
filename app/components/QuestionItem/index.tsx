736c9664db24843202948f01d8527db2d92633d8
hakushun <hakushun.pianist@gmail.com>
2024-11-10 14:12:03 +0900

    style: absoluteをやめてgridに

--- app/components/QuestionItem/index.tsx
+++ app/components/QuestionItem/index.tsx
@@ -45,33 +45,35 @@ export const QuestionItem = forwardRef<HTMLLIElement, Props>(function (
       tabIndex={-1}
       onClick={() => handleFocus(question.id)}
       onKeyDown={handleKeyDown}>
-      <fieldset className={styles.fieldset}>
-        <legend className={styles.legend}>
-          <SpeechButton label="設問文を読み上げる" onClick={startSpeech} />
-          <h3 ref={ref}>
-            <TextWithRuby texts={question.title} />
-          </h3>
-        </legend>
-        <div className={styles.options}>
-          {question.options.map((option) => (
-            <label
-              key={option.value}
-              className={styles.option}
-              onClick={(e) => e.stopPropagation()}>
-              <input
-                type="radio"
-                className={styles.radio}
-                name={`${question.id}`}
-                value={option.value}
-                checked={checkedValue === option.value}
-                onChange={() => handleChange(option.value, question.id)}
-                required
-              />
-              <TextWithRuby texts={option.label} />
-            </label>
-          ))}
-        </div>
-      </fieldset>
+      <div className={styles.inner}>
+        <fieldset className={styles.fieldset}>
+          <legend className={styles.legend}>
+            <SpeechButton label="設問文を読み上げる" onClick={startSpeech} />
+            <h3 ref={ref}>
+              <TextWithRuby texts={question.title} />
+            </h3>
+          </legend>
+          <div className={styles.options}>
+            {question.options.map((option) => (
+              <label
+                key={option.value}
+                className={styles.option}
+                onClick={(e) => e.stopPropagation()}>
+                <input
+                  type="radio"
+                  className={styles.radio}
+                  name={`${question.id}`}
+                  value={option.value}
+                  checked={checkedValue === option.value}
+                  onChange={() => handleChange(option.value, question.id)}
+                  required
+                />
+                <TextWithRuby texts={option.label} />
+              </label>
+            ))}
+          </div>
+        </fieldset>
+      </div>
     </li>
   );
 });
