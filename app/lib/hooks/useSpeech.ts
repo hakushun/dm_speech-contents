8e908bc0b6c26560411be7a8d8d353e09d74817c
hakushun <hakushun.pianist@gmail.com>
2024-11-10 13:10:24 +0900

    refactor: ルビはスピーチ対象から外す

--- app/lib/hooks/useSpeech.ts
+++ app/lib/hooks/useSpeech.ts
@@ -7,17 +7,26 @@ export function useSpeech<T extends HTMLElement>() {
   const ref = useRef<T | null>(null);
   const speechSpeed = useSpeechStore((state) => state.speechSpeed);
 
-  const startSpeech = () => {
+  function getTextWithoutRuby(element: T) {
+    const clone = element.cloneNode(true) as T;
+    const rtElements = clone.getElementsByTagName('rt');
+    Array.from(rtElements).forEach((rt) => rt.remove());
+    return clone.innerText;
+  }
+
+  function startSpeech() {
+    window.speechSynthesis.cancel();
+
     if (!ref.current) return;
 
-    const utterance = new SpeechSynthesisUtterance(ref.current.innerText);
+    const utterance = new SpeechSynthesisUtterance(getTextWithoutRuby(ref.current));
 
     utterance.lang = 'ja-JP';
 
     utterance.rate = speechSpeed;
 
     window.speechSynthesis.speak(utterance);
-  };
+  }
 
   return { ref, startSpeech };
 }
